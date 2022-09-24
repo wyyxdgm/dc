8c28b8b688f1ad2e4953a55d407fcb32e9c50de9
Damo <wyyxdgm@163.com>
2022-09-24 20:06:53 +0800

    complete

--- dcb.js
+++ dcb.js
@@ -2,23 +2,38 @@ const fs = require("fs");
 const path = require("path");
 let txt = fs.readFileSync(path.join(__dirname, "dc.txt")).toString();
 // txt = '\n2370 assurance \n[əˈʃʊərəns] \nn.确保,断言; \n保证,担保 \nHe has a life assurance. \n他投保了人寿保险。 \n2371 bankrupt \n[ˈbæŋkrʌpt] \na.破产的 \nIf the firm cannot sell its products, it will \ngo bankrupt. \n如果公司的产品卖不出去,它就会倒闭。\n'
-let regexp = /(\d+)[.]?\s?([~+\w]+)/g; ///(\d+)\s(\w+)\s?\n+\[[^\]]+\]/g;
+let regexp = /(\d+)[.]?\s?([~+\w\ ]+)/g; ///(\d+)\s(\w+)\s?\n+\[[^\]]+\]/g;
 let re = null;
 let arr = new Array(2371 + 1);
+// let duplicate = [];
+let worlds = new Map();
 let ppre = 0;
 while ((re = regexp.exec(txt))) {
-  if (ppre + 1 == re[1]) {
-    arr[re[1]] = re.slice(0, 6);
+  let currentNumber = re[1];
+  if (ppre + 1 == currentNumber) {
+    arr[currentNumber] = re.slice(0, 6);
     ppre++;
+    let world = re[2];
+    if (worlds.has(world)) {
+      worlds.get(world).push(currentNumber);
+    }
+    else worlds.set(world, []);
   }
 }
+worlds.forEach((v, k) => {
+  if (v.length > 1) console.log('duplicate', k, v);
+});
+console.log('duplicate');
 // console.log('数据-------------------');
 // console.log(arr);
 console.log('缺失index--------------');
 let eArr = [];
-for (let index = 0; index < arr.length; index++) {
+for (let index = 1; index < arr.length; index++) {
   const element = arr[index];
-  if (!element) eArr.push(index);
+  if (!element) {
+    eArr.push(index);
+  }
+
 }
 console.log(eArr);
 // let arr2 = arr.map((i, idx) => !i ? idx : false).filter(i => !!i);
@@ -29,8 +44,11 @@ console.log(eArr);
 //   if (element[1] + 1 != element2[1]) console.log();
 // }
 
+arr.splice(0, 1)
+console.log('单词-arr------------------');
+console.log(arr);
 console.log('单词-------------------');
-console.log(arr.map(i => i[2]).join(','))
+console.log(arr.map(i => i[2].trim()).join(', '))
 
 let group = [];
 let groupItem = null;
@@ -39,5 +57,5 @@ while ((groupItem = arr.splice(0, 500)).length) {
 }
 group.forEach((gArr, idx) => {
   console.log(`分组${idx + 1},个数${gArr.length}-------------------`);
-  console.log(gArr.map(i => i[2]).join(','))
+  console.log(gArr.map(i => i[2].trim()).join(','))
 })
